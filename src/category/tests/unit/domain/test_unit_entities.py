d72dc91fdb70aa4ca3e67623a0128969241b3971
kcarww <carlos.oe.cc@gmail.com>
2024-08-05 18:00:15 -0300

    feat: make tests without calling validate method again

--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -1,6 +1,8 @@
 import unittest
 from datetime import datetime
 from dataclasses import FrozenInstanceError, is_dataclass
+from unittest import mock
+from unittest.mock import patch
 from category.domain.entities import Category
 
 
@@ -9,43 +11,51 @@ class TestCategoryUnit(unittest.TestCase):
         self.assertTrue(is_dataclass(Category))
 
     def test_constructor(self):
-        data = datetime.now()
-        category = Category(
-            name='Movies', description='description', is_active=True, created_at=data)
-        self.assertEqual(category.name, 'Movies')
-        self.assertEqual(category.description, 'description')
-        self.assertEqual(category.is_active, True)
-        self.assertEqual(category.created_at, data)
+        with patch.object(Category, 'validate') as mock_validate_method:
+            data = datetime.now()
+            category = Category(
+                name='Movies', description='description', is_active=True, created_at=data)
+            mock_validate_method.assert_called_once()
+            self.assertEqual(category.name, 'Movies')
+            self.assertEqual(category.description, 'description')
+            self.assertEqual(category.is_active, True)
+            self.assertEqual(category.created_at, data)
 
-        category = Category(name='Movies')
-        self.assertEqual(category.name, 'Movies')
-        self.assertEqual(category.description, None)
-        self.assertEqual(category.is_active, True)
-        self.assertIsInstance(category.created_at, datetime)
+            category = Category(name='Movies')
+            self.assertEqual(category.name, 'Movies')
+            self.assertEqual(category.description, None)
+            self.assertEqual(category.is_active, True)
+            self.assertIsInstance(category.created_at, datetime)
 
     def test_if_created_at_is_generated_in_constructor(self):
-        category1 = Category(name='Movies')
-        category2 = Category(name='Movies')
-        self.assertNotEqual(category1.created_at.timestamp(),
-                            category2.created_at.timestamp())
+        with patch.object(Category, 'validate') as mock_validate_method:
+            category1 = Category(name='Movies')
+            mock_validate_method.assert_called_once()
+            category2 = Category(name='Movies')
+            self.assertNotEqual(category1.created_at.timestamp(),
+                                category2.created_at.timestamp())
 
     def test_is_immutable(self):
-        with self.assertRaises(FrozenInstanceError):
-            value_object = Category(name='teste')
-            value_object.name = 'category'
+        with patch.object(Category, 'validate'):
+            with self.assertRaises(FrozenInstanceError):
+                value_object = Category(name='teste')
+                value_object.name = 'category'
 
     def test_activate(self):
-        category = Category(name='Movies', is_active=False)
-        category.activate()
-        self.assertTrue(category.is_active)
+        with patch.object(Category, 'validate'):
+            category = Category(name='Movies', is_active=False)
+            category.activate()
+            self.assertTrue(category.is_active)
 
     def test_deactivate(self):
-        category = Category(name='Movies')
-        category.deactivate()
-        self.assertFalse(category.is_active)
+        with patch.object(Category, 'validate'):
+            category = Category(name='Movies')
+            category.deactivate()
+            self.assertFalse(category.is_active)
 
     def test_update(self):
-        category = Category(name='Movies')
-        category.update(name='Games', description='description')
-        self.assertEqual(category.name, 'Games')
-        self.assertEqual(category.description, 'description')
+        with patch.object(Category, 'validate'):
+            category = Category(name='Movies')
+            category.update(name='Games', description='description')
+            self.assertEqual(category.name, 'Games')
+            self.assertEqual(category.description, 'description')
