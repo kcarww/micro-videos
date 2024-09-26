1d51e4f73a95506eb2c0c5d238c852eb7043aa5f
kcarww <carlos.oe.cc@gmail.com>
2024-09-26 20:35:03 -0300

    [F2416] feat: create category use case

---
+++ src/category/application/use_cases.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import datetime
+from category.domain.entities import Category
+from category.domain.repositories import CategoryRepository
+
+@dataclass(slots=True, frozen=True)
+class CreateCategoryUseCase:
+    
+    category_repo: CategoryRepository
+    
+    def execute(self, input_param: 'Input') -> 'Output':
+        category = Category(name=input_param.name,
+                            description=input_param.description,
+                            is_active=input_param.is_active)
+        self.category_repo.insert(category)
+        return self.Output(
+            id=category.id,
+            name=category.name,
+            description=category.description,
+            is_active=category.is_active,
+            created_at=category.created_at
+        )
+    
+    @dataclass(slots=True, frozen=True)
+    class Input:
+        name: str
+        description: Optional[str]
+        is_active: Optional[bool]
+        
+    @dataclass(slots=True, frozen=True)
+    class Output:
+        id: str
+        name: str
+        description: Optional[str]
+        is_active: bool
+        created_at: datetime
