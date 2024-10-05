51f5ad2c6e1f5112fc636cb234b89b0fcee3feed
kcarww <carlos.oe.cc@gmail.com>
2024-10-05 10:21:09 -0300

    [F2486] feat: search category django repository

--- src/core/category/infra/django_app/repositories.py
+++ src/core/category/infra/django_app/repositories.py
@@ -2,6 +2,7 @@
 
 from typing import List
 from django.core import exceptions as django_exceptions
+from django.core.paginator import Paginator
 from core.__seedwork.domain.exceptions import NotFoundException
 from core.__seedwork.domain.value_objects import UniqueEntityId
 from core.category.domain.entities import Category
@@ -13,6 +14,9 @@ from core.category.infra.django_app.models import CategoryModel
 
 
 class CategoryDjangoRepository(CategoryRepository):
+
+    sortable_fields: List[str] = ['name', 'created_at']
+
     def insert(self, entity: Category) -> None:
         model = CategoryModelMapper.to_model(entity)
         model.save()
@@ -29,7 +33,7 @@ class CategoryDjangoRepository(CategoryRepository):
         self._get(entity.id)
         model = CategoryModelMapper.to_model(entity)
         model.save()
-        
+
     def delete(self, entity_id: str | UniqueEntityId) -> None:
         model = self._get(str(entity_id))
         model.delete()
@@ -42,4 +46,28 @@ class CategoryDjangoRepository(CategoryRepository):
                 f"Entity not found using ID '{entity_id}'") from exception
 
     def search(self, input_params: CategoryRepository.SearchParams) -> CategoryRepository.SearchResult:
-        raise NotImplementedError()
+        query = CategoryModel.objects.all()
+
+        if input_params.filter:
+            query = query.filter(name__icontains=input_params.filter)
+
+        if input_params.sort and input_params.sort in self.sortable_fields:
+            query = query.order_by(
+                input_params.sort if input_params.sort_dir == 'asc' else f"-{input_params.sort}")
+            
+        else:
+            query = query.order_by('-created_at')
+
+        
+        paginator = Paginator(query, input_params.per_page)
+        page_obj = paginator.page(input_params.page)
+        
+        return CategoryRepository.SearchResult(
+            items=[CategoryModelMapper.to_entity(model) for model in page_obj.object_list],
+            total=paginator.count,
+            current_page=input_params.page,
+            per_page=input_params.per_page,
+            sort=input_params.sort,
+            sort_dir=input_params.sort_dir,
+            filter=input_params.filter
+        )
