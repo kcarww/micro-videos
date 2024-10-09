190ca0347c0bee8751c23b252ee902871aed8652
kcarww <carlos.oe.cc@gmail.com>
2024-10-09 18:29:07 -0300

    [F2537] feat: created body fixture for invalid category

--- src/core/category/tests/fixture/categories_api_fixture.py
+++ src/core/category/tests/fixture/categories_api_fixture.py
@@ -1,7 +1,7 @@
 
 from dataclasses import dataclass
-from typing import Any
-
+from typing import Any, Optional
+from rest_framework.exceptions import ErrorDetail, ValidationError
 import pytest
 
 from core.category.domain.entities import Category
@@ -20,7 +20,115 @@ class Response:
 @dataclass
 class HttpExpect:
     request: Request
-    response: Response
+    response: Optional[Response] = None
+    exception: Optional[Exception] = None
+
+
+
+@dataclass
+class CategoryInvalidBodyFixture:
+    body_empty: HttpExpect
+    name_none: HttpExpect
+    name_empty: HttpExpect
+    name_not_a_str: HttpExpect
+    description_not_a_str: HttpExpect
+    is_active_none: HttpExpect
+    is_active_empty: HttpExpect
+    is_active_not_a_bool: HttpExpect
+
+    @staticmethod
+    def arrange():
+        faker = Category.fake().a_category()
+        return CategoryInvalidBodyFixture(
+            body_empty=HttpExpect(
+                request=Request(body={}),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('This field is required.', 'required')
+                    ],
+                })
+            ),
+            name_none=HttpExpect(  # vai acontecer um erro
+                request=Request(
+                    body={'name': faker.with_invalid_name_none().name}),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('This field may not be null.', 'null')
+                    ],
+                })),
+            name_empty=HttpExpect(
+                request=Request(
+                    body={'name': faker.with_invalid_name_empty().name}),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('This field may not be blank.', 'blank')
+                    ],
+                })),
+            name_not_a_str=HttpExpect(
+                request=Request(
+                    body={'name': faker.with_invalid_name_not_a_string().name}),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('Not a valid string', 'invalid')
+                    ],
+                })),
+            description_not_a_str=HttpExpect(
+                request=Request(
+                    body={
+                        'description': faker.with_invalid_description_not_a_string().description
+                    }
+                ),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('This field is required.', 'required')
+                    ],
+                    'description': [
+                        ErrorDetail('Not a valid string', 'invalid')
+                    ],
+                })),
+            is_active_none=HttpExpect(
+                request=Request(
+                    body={
+                        'is_active': faker.with_invalid_is_active_none().is_active
+                    }
+                ),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('This field is required.', 'required')
+                    ],
+                    'is_active': [
+                        ErrorDetail('This field may not be null.', 'null')
+                    ],
+                })),
+            is_active_empty=HttpExpect(
+                request=Request(
+                    body={
+                        'is_active': faker.with_invalid_is_active_empty().is_active
+                    }
+                ),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('This field is required.', 'required')
+                    ],
+                    'is_active': [
+                        ErrorDetail('Must be a valid boolean.', 'invalid')
+                    ],
+                })),
+            is_active_not_a_bool=HttpExpect(
+                request=Request(
+                    body={
+                        'is_active': faker.with_invalid_is_active_not_a_boolean().is_active
+                    }
+                ),
+                exception=ValidationError({
+                    'name': [
+                        ErrorDetail('This field is required.', 'required')
+                    ],
+                    'is_active': [
+                        ErrorDetail('Must be a valid boolean.', 'invalid')
+                    ],
+                })),
+        )
 
 
 class CategoryApiFixture:
